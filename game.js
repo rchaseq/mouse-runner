126b80d13059d98350e60e7767dc931baeee29e5
Rowan Quinn <35435338+rchaseq@users.noreply.github.com>
2024-01-19 12:47:52 -0500

    Update game.js
    Correcting the highestScore function

--- game.js
+++ game.js
@@ -132,7 +132,7 @@ class playGame extends Phaser.Scene{
         function saveHighestScore(score) {
             if (score > highestScore) {
                 highestScore = score;
-                localStorage.setItem('highestScore', highestScore);
+                localStorage.setItem('highestScore', highestScore - 2); //Top score otherwise adds 1-3 points to the score before saving, unsure why...this will balance it out until I figure it out
             }
         };
 
